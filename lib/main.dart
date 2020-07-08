fb0433235cc1c9cafe807f72a32e4d775cca8b17
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-07-08 11:52:12 +0300

    padding_and_column

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,49 @@ class Profil extends StatelessWidget {
       appBar: AppBar(
         title: Text("Profil"),
         centerTitle: true,
+        backgroundColor: Color.fromRGBO(10, 189, 149, 1),
+      ),
+      backgroundColor: Color.fromRGBO(255, 222, 99, 1),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "ISIM",
+              style: TextStyle(
+                  color: Color.fromRGBO(190, 173, 121, 1), letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Emral",
+              style: TextStyle(
+                  color: Color.fromRGBO(155, 155, 155, 1),
+                  letterSpacing: 2.0,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+            Text(
+              "SOYISIM",
+              style: TextStyle(
+                  color: Color.fromRGBO(190, 173, 121, 1), letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Yılmaz",
+              style: TextStyle(
+                  color: Color.fromRGBO(155, 155, 155, 1),
+                  letterSpacing: 2.0,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
