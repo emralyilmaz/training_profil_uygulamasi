8d408d142042813abdc197a75532dddd1759d0e4
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-07-08 15:40:52 +0300

    AppBar_Actions

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ void main() {
   ));
 }
 
-class Profil extends StatelessWidget {
+class Profil extends StatefulWidget {
+  @override
+  _ProfilState createState() => _ProfilState();
+}
+
+class _ProfilState extends State<Profil> {
+  int uyelikLevel = 3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +20,22 @@ class Profil extends StatelessWidget {
         title: Text("Profil"),
         centerTitle: true,
         backgroundColor: Color.fromRGBO(204, 122, 101, 1),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.keyboard_arrow_up),
+              onPressed: () {
+                setState(() {
+                  uyelikLevel++;
+                });
+              }),
+          IconButton(
+              icon: Icon(Icons.keyboard_arrow_down),
+              onPressed: () {
+                setState(() {
+                  uyelikLevel--;
+                });
+              })
+        ],
       ),
       backgroundColor: Color.fromRGBO(230, 218, 166, 1),
       body: Padding(
@@ -58,6 +80,23 @@ class Profil extends StatelessWidget {
             ),
             Text(
               "Yılmaz",
+              style: TextStyle(
+                  color: Color.fromRGBO(57, 33, 67, 1),
+                  letterSpacing: 2.0,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+            Text(
+              "UYELİK SEVİYESİ",
+              style: TextStyle(
+                  color: Color.fromRGBO(166, 175, 146, 1), letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "$uyelikLevel",
               style: TextStyle(
                   color: Color.fromRGBO(57, 33, 67, 1),
                   letterSpacing: 2.0,
